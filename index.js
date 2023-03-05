e9f192bd5b3f944cf957a18b8ee64756a46f06cf
Fulvio Salza <fsalza96@gmail.com>
2023-03-04 22:44:58 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement("h1")
+
+newHeader.setAttribute('id', 'victory');
+
+document.getElementById("victory");
+newHeader.innerHTML = "<h1>Fulvio is the champion!</h1>"
